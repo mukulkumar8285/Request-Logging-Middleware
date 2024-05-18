3659530a66b2024de90e2131c9f98eae713494ac
mukulkumar8285 <149408801+mukulkumar8285@users.noreply.github.com>
2024-05-18 23:29:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+// Request logging middleware
+function requestLogger(req, res, next) {
+    const start = Date.now();
+    res.on('finish', () => {
+        const duration = Date.now() - start;
+        console.log(`${req.method} ${req.originalUrl} - ${res.statusCode} [${duration}ms]`);
+    });
+    next();
+}
+
+app.use(requestLogger);
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
